6e73567e2bcdbb2a1751cbcbe60695570cfc35b4
github username <github email address>
2020-03-25 13:30:56 -0700

    login function add

--- src/index.js
+++ src/index.js
@@ -11,19 +11,13 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 });
 
-function fetchUser(userId) {
-  return fetch(`${endPoint}/${userId}`)
-      .then(res => res.json())
-      .catch(console.error);
-}
-
 function renderRoom(rootContainer, roomObj) {
   const roomContainer = document.createElement('div');
   roomContainer.innerText = roomObj.name;
   for(let storage of roomObj.storages) {
     renderStorage(roomContainer, storage);
   }
-
+  
   rootContainer.appendChild(roomContainer);
 }
 
@@ -40,4 +34,20 @@ function renderItem(storageContainer, itemObj) {
   const itemContainer = document.createElement('p');
   itemContainer.innerHTML = itemObj.name;
   storageContainer.appendChild(itemContainer);
+}
+
+// Login
+function loginForm() {
+  let loginForm = document.getElementById('login-form') //assign html form.id 'login-form'
+  loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let user = {name: event.target.name.value}
+    fetchUser(user);
+  })
+}
+
+function fetchUser(userId) {
+  return fetch(`${endPoint}/${userId}`)
+      .then(res => res.json())
+      .catch(console.error);
 }
