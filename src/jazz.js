f576d0bfccf9edf8f9222276c9b7077e8589311f
github username <github email address>
2020-03-25 14:52:11 -0700

    adding sign up and fetch post

--- src/jazz.js
+++ src/jazz.js
@@ -1,3 +1,4 @@
+//  Login
 function loginForm() {
     let loginForm = document.getElementById('login-form') // assign form.id
     loginForm.addEventListener('submit', function(event) {
@@ -13,3 +14,31 @@ function fetchUser(userId) {
         .catch(console.error);  
   }
   
+//  Sign Up
+//  Need to locate proper elents
+function signUpForm() {
+    const signUpForm = document.getElementById('sign-up-form')
+    signUpForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        let user = {name: event.target.name.value}
+        postUesr(user);
+    })
+}
+
+function postUser(user) {
+    fetch(endPoint, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data) {
+           let currentUser = data
+        })
+        .catch(console.error);  
+}
