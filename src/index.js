1b17d917c8138e0dce957b4e2eba8de6fd343fc8
github username <github email address>
2020-03-25 01:03:37 -0700

    working code for fetching user

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
+const endPoint = "http://localhost:3000/api/v1/users"
+
 document.addEventListener('DOMContentLoaded', () => {
-    alert('LOADED');
-    const endPoint = '';
-    fetch(endPoint)
+  const roomList = document.querySelector('#room-list');
+  // TODO: @nk2303 - Use user id to fetch data. Temporarily hard code using user 1.
+  fetchUser(1)
+  .then(res => {
+    for(let room of res.rooms) {
+      renderRoom(roomList, room);
+    }
+  })
+});
+
+function fetchUser(userId) {
+  return fetch(`${endPoint}/${userId}`)
       .then(res => res.json())
-      .then(json => console.log(json));
-  });
+      .catch(console.error);
+}
 
+function renderRoom(rootContainer, roomObj) {
+  const roomContainer = document.createElement('div');
+  roomContainer.innerText = roomObj.name;
+  for(let storage of roomObj.storages) {
+    renderStorage(roomContainer, storage);
+  }
 
+  rootContainer.appendChild(roomContainer);
+}
+
+function renderStorage(roomContainer, storageObj) {
+  const storageContainer = document.createElement('div');
+  storageContainer.innerText = storageObj.name;
+  for (let item of storageObj.items) {
+    renderItem(storageContainer, item);
+  }
+  roomContainer.appendChild(storageContainer);
+}
+
+function renderItem(storageContainer, itemObj) {
+  const itemContainer = document.createElement('p');
+  itemContainer.innerHTML = itemObj.name;
+  storageContainer.appendChild(itemContainer);
+}
